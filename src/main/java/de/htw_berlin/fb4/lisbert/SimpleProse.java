17a3d8cb9a8e0536ef0e56747057587cfa60da58
Lisbert01 <lisa.hausenblas@protonmail.com>
2026-07-04 11:56:39 +0200

    add a header with Copyright und LiCense info
    Added copyright notice and license information.

--- src/main/java/de/htw_berlin/fb4/lisbert/SimpleProse.java
+++ src/main/java/de/htw_berlin/fb4/lisbert/SimpleProse.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2026 Lisa Hausenblas
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE.txt.
+ */
+
 package de.htw_berlin.fb4.lisbert;
 
 import de.htw_berlin.fb4.ossd.prose.Prose;
